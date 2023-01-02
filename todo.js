1b8c8d57a10f070781b3068d510f9c20cc9a58d4
Luis Gomes D. Neto <83004652+Lusigmes@users.noreply.github.com>
2023-01-02 13:33:55 -0300

    Lista de Todos
    Protótipo ToDo list

---
+++ todo.js
@@ -0,0 +1,152 @@
+const { response } = require("express");
+const express = require("express");
+const cors = require("cors");
+const { v4: uuidv4 } = require("uuid");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const users = [];
+
+/****************************** MIDDLEWARE **********************************/
+
+function verificarConta(request,response,next){
+
+    const { username } = request.headers;
+
+    const user = users.find(user => user.username === username);
+
+    if(!user){
+        return response.status(404).json(
+            {error: "ERRO DE VERIFICAÇÃO DE CONTA!"});
+    }
+
+    request.user = user;
+
+    return next();
+}
+
+/*****************************************************************************/
+
+//CADASTRAR USUARIO [X]
+app.post("/users", (request,response)=>{
+    //const { user } = require;
+    const { name, username } = request.body;
+    
+    const verificarUsername = 
+        users.some((user)=> user.username === username);
+
+    if(verificarUsername){
+        return response.status(400).json(
+            {error: "Usuário já cadastrado!"});
+    }
+    
+    const user = {
+        id: uuidv4(),
+        name,
+        username,
+        todos:[]
+    };
+
+
+    users.push(user)
+
+    //console.log(users);
+
+    return response.status(201).json(user);
+});
+
+//CRIAR TODO [X]
+app.post("/todos", verificarConta ,(request,response)=>{
+    const {user} = request;
+
+    const {titulo,deadline} = request.body;
+
+    const todo = {
+        id: uuidv4(),
+        titulo,
+        done:false,
+        deadline: new Date(deadline),
+        criado_em: new Date()
+    }
+    
+    user.todos.push(todo);
+    //  console.log(user.todo);
+
+    return response.status(201).json(user.todos);
+});
+//app.use(verificarConta);
+
+//LISTAR TODO [X]
+app.get("/todos", verificarConta, (request,response)=>{
+    const { user } = request;
+
+    //console.log(user.todo);
+
+    return response.json(user.todos);
+});
+//LISTAR USERS [X]
+app.get("/users", verificarConta, (request,response)=>{
+    const {user} = request;
+
+    return response.json(users);
+});
+
+//ATUALIZAR TODO [x]
+app.put("/todos/:id",verificarConta,  (request,response)=>{
+    const {user} = request;
+    const {titulo,deadline} = request.body;
+    const {id} = request.params;
+
+    const todo = user.todos.find(todo => todo.id === id);
+    if(!todo){
+        return response.status(404).json({
+            error: "ToDo NÃO ENCONTRADO!"
+        });
+    }
+
+    todo.titulo = titulo;
+    todo.deadline = new Date(deadline);
+
+    return response.status(200).json(todo);
+});
+
+//ATUALIZAR TODO {FEITO} [x]
+app.patch("/todo/:id/done",verificarConta,  (request,response)=>{
+    const {user}  = request;
+    const {id} = request.params;
+
+    const todo = user.todos.find(todo => todo.id === id);
+    if(!todo){
+        return response.status(404).json({
+            error: "ToDo NÃO ENCONTRADO!"
+        });
+    }
+    
+    todo.done = true;
+        
+    return response.status(200).json(todo);
+});
+
+//EXCLUIR TODO []
+app.delete("/todos/:id",verificarConta, (request,response)=>{
+    const {user} = request;
+    const {id} = request.params;
+
+    const todoI = user.todos.findIndex(todo => todo.id === id);
+    if(todoI === -1){
+        return response.status(404).json({
+            error: "Todo NÃO ENCONTRADO!"
+        });
+    }
+    user.todos.splice(todoI,1);
+
+    return response.status(204).json();
+});
+
+
+
+app.listen(3333);
+module.exports = app;
